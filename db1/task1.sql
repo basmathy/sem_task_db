5546a701243cfc482baa923d32e58c4fbdcf0588
Lidia <lshvetsova_a@icloud.com>
2026-04-03 10:13:12 +0500

    Update and rename task1 to task1.sql

--- db1/task1.sql
+++ db1/task1.sql
@@ -4,10 +4,9 @@ from motorcycle m
 -- соединение по модели
 left join vehicle v on m.model = v.model 
 
--- основные фильтры
+-- фильтруем по мощности, цене и типу
 where m.horsepower > 150 
 and m.price < 20000 
 and m.type = 'Sport'
 
--- сортировка
 order by m.horsepower desc
